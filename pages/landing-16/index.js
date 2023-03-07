65182001f39aa1430f71c9d72a41c4c60c55cd8b
Md. Jafrul Hasan Rasel <73773576+jhrasel@users.noreply.github.com>
2023-03-07 15:12:35 +0600

    Running Working

---
+++ pages/landing-16/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { LandingSixteen } from '../../Components/LandingSixteen/LandingSixteen'
+
+const index = () => {
+
+  return (
+
+    <div>
+       <LandingSixteen></LandingSixteen>
+    </div>
+
+  )
+}
+
+export default index
---
+++ pages/landing-17/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { LandingSeventeen } from '../../Components/LandingPage/LandingSeventeen/LandingSeventeen'
+
+const index = () => {
+
+  return (
+
+    <div>
+       <LandingSeventeen></LandingSeventeen>
+    </div>
+
+  )
+}
+
+export default index
